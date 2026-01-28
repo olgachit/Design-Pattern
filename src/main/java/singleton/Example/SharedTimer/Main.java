e0c7fe546acd914256c9240183c70da2d73613af
Olga Chitembo <olga.chitembo@metropolia.fi>
2026-01-28 09:52:39 +0200

    Example: Shared timer

---
+++ src/main/java/singleton/Example/SharedTimer/Main.java
@@ -0,0 +1,8 @@
+package singleton.Example.SharedTimer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Gui.launch(Gui.class);
+    }
+}
---
+++ src/main/java/singleton/Example/SharedTimer/Timer.java
@@ -0,0 +1,25 @@
+package singleton.Example.SharedTimer;
+
+public class Timer {
+    private static Timer instance;
+    private long startTime;
+    private Timer() {
+        reset();
+    }
+
+    public static synchronized Timer getInstance() {
+        if (instance == null) {
+            instance = new Timer();
+        }
+        return instance;
+    }
+
+    public synchronized void reset() {
+        startTime = System.currentTimeMillis();
+    }
+    public synchronized int getTime() {
+        long currentTime = System.currentTimeMillis();
+        long elapsedTime = currentTime - startTime;
+        return (int) (elapsedTime / 1000);
+    }
+}
